74a76f038b6770826a9e5d6dc93ea8a01349799e
xiaoh <seanxduan@gmail.com>
2025-08-05 11:34:20 -0500

    Starting to build things in Django to increase my project density and show my skills.

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
 
 from . import views
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
 ]
