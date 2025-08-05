ebaeaaec2f8463ac43cfae9e3df94c6e78010357
xiaoh <seanxduan@gmail.com>
2025-08-05 11:24:53 -0500

    Starting to build things in Django to increase my project density and show my skills.

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,7 @@
+from http.client import HTTPResponse
+
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    return HTTPResponse("Hello Sean or Friends of Sean. It's Polls-ening Time!")
